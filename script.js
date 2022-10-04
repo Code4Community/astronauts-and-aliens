8af000092da1c2a27fcef0e4b235d6ba5b0bf717
acou12 <notarealemailaddress@fake.com>
2022-10-03 20:58:15 -0400

    Add very basic bullets

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//Screen Seize and Camera
+// Screen size and camera
 var screenWidth = 800;
-var scrollWidth = 2*screenWidth; // width of the rolling screen
+var scrollWidth = 2 * screenWidth; // width of the rolling screen
 var screenHeight = 600;
 
 var config = {
@@ -11,55 +11,90 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 0 },
             debug: false
         }
     },
     scene: {
-        preload: preload,
-        create: create,
-        update: update
+        preload,
+        create,
+        update
     }
 };
+
+class Bullet extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x, y, 'bullet')
+        scene.add.existing(this)
+        this.setScale(0.1, 0.1)
+        scene.physics.add.existing(this)
+        this.setCollideWorldBounds(true)
+    }
+}
 
 class Spaceship extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
         
-        //When we determine the file name of the sprite for spaceship we need
-        //to replace 'Spaceship' with the file name
+        // When we determine the file name of the sprite for spaceship we need
+        // to replace 'Spaceship' with the file name
         super(scene, x, y, 'Spaceship');
         scene.add.existing(this);
         scene.physics.add.existing(this);
-        this.setScale(0.8, 0.8);
+        this.setScale(0.3, 0.3);
         this.setBounce(0.3);
         this.setCollideWorldBounds(true);
-        scene.physics.add.collider(this, platforms);
 
-
-        //alive is when the spaceship is alive
+        // alive is when the spaceship is alive
         this.alive = true;
     }
 }
 
-// Function to allow entity types to move left
-Entity.prototype.moveLeft = function() {
+Spaceship.prototype.vx = 0
+Spaceship.prototype.vy = 0
+
+Spaceship.prototype.moveUp = function() {
+    this.setVelocityY(-140);
+    this.vy = -140
+    // var that = this;
+    // setTimeout(function() {
+    //     that.setVelocityY(0);
+    // }, 2000);
+}
+
+Spaceship.prototype.moveDown = function() {
+    this.setVelocityY(140);
+    this.vy = 140
+    // var that = this;
+    // setTimeout(function() {
+    //     that.setVelocityY(0);
+    // }, 2000);
+}
+
+Spaceship.prototype.moveLeft = function() {
     this.setVelocityX(-140);
-   var that = this;
-    setTimeout(function() {
-        that.setVelocityX(0);
-    }, 2000);
+    this.vx = -140
+    // var that = this;
+    // setTimeout(function() {
+    //     that.setVelocityX(0);
+    // }, 2000);
 }
 
-// Function to allow entity types to move right
-Entity.prototype.moveRight = function() {
+Spaceship.prototype.moveRight = function() {
     this.setVelocityX(140);
-    var that = this;
-    setTimeout(function() {
-        that.setVelocityX(0);
-    }, 2000);
+    this.vx = 140
+    // var that = this;
+    // setTimeout(function() {
+    //     that.setVelocityX(0);
+    // }, 2000);
 }
 
-//Define the game
+Spaceship.prototype.shoot = function (angle) {
+    const bullet = new Bullet(this.scene, this.x, this.y)
+    bullet.setVelocityX(200 * Math.cos(angle / 360 * 2 * Math.PI))
+    bullet.setVelocityY(200 * Math.sin(angle / 360 * 2 * Math.PI))
+}
+
+// Define the game
 var game = new Phaser.Game(config);
 
 function preload() {
@@ -73,13 +108,24 @@ function preload() {
     this.load.image('num2', 'assets/numbers/number2.png');
     this.load.image('num3', 'assets/numbers/number3.png');
     this.load.image('Spaceship', 'assets/Spaceship.png');
+    this.load.image('bullet', 'assets/bullet.png');
 }
 
-function create() {
+let spaceship;
 
+function create() {
+    spaceship = new Spaceship(this, 400, 400)
+    this.input.keyboard.on('keydown_W', () => spaceship.moveUp(), this);
+    this.input.keyboard.on('keydown_A', () => spaceship.moveLeft(), this);
+    this.input.keyboard.on('keydown_S', () => spaceship.moveDown(), this);
+    this.input.keyboard.on('keydown_D', () => spaceship.moveRight(), this);
+    this.input.keyboard.on('keydown_Q', () => spaceship.shoot(45), this);
 }
 
 function update() {
-
+    spaceship.vx *= 0.98
+    spaceship.vy *= 0.98
+    spaceship.setVelocityX(spaceship.vx)
+    spaceship.setVelocityY(spaceship.vy)
 }
 
